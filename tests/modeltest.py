d3e1044ffef23ea4c201a5f7dcc83a89c6effb99
Paul Upendo <paul.upendo@andela.com>
2017-03-18 16:53:36 +0300

    Create Name tests

--- tests/modeltest.py
+++ tests/modeltest.py
@@ -2,6 +2,28 @@ import unittest
 
 
 class TestModel(unittest.TestCase):
+    def setUp(self):
+        amity = Amity()
+        rooms = Rooms()
+        staff = Staff()
+        fellow = Fellow()
+        staff = Staff()
+        person = Person()
+
+    # Test for appropriate class names
+    def test_Amity(self):
+        self.assertEqual('Amity', amity.getName())
+
+    def test_Person(self):
+        self.assertEqual('Person', person.getName())
+
+    def test_Rooms(self):
+        self.assertEqual('Rooms', rooms.getName())
+
+    def test_Staff(self):
+        self.assertEqual('Staff', rooms.getName())
+
+    # Tests to check if classes correctly inherit
 
     def test_FellowInherit(self):
         issubclass(Person, Fellow)
