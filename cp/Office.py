b81083f4d6873a97831adef88bed5b191816a77d
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-04-06 15:31:33 +0300

    Delete old folder structure

--- cp/Office.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '/Users/Upendo/Documents/CP1/CP1-amity-allocation')
-from cp.Rooms import Rooms
-
-
-class Office(Rooms):
-    pass
--- cp/Rooms.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '/Users/Upendo/Documents/CP1/CP1-amity-allocation')
-
-
-class Rooms():
-    def room_space(self, room_name):
-        self.room_name = []
-        return self.room_name
