0e01078619f57797beb2e6d5193f9e465ea9804b
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-04-07 13:23:13 +0300

    Implement random allocation.#1.0

--- app/cp/amity.py
+++ app/cp/amity.py
@@ -77,7 +77,7 @@ class Amity():
             Person_id = self.first_name + prefix + str(suffix)
             return Person_id
 
-    def allocate_room_randomly(self, list_arg):
+    def allocate_room_randomly(self, list_arg=None):
         """
         - Have people to be allocated in a list
         - Randomly chooose a person from the list in a load_people
@@ -87,8 +87,30 @@ class Amity():
         - Proceed to next face
 
         """
-
-        pass
+        if self.Role.upper() == "FELLOW":
+            for fellow in self.fellow_info:
+                list_arg = self.fellow_info
+                selected_fellow = random.choice(list_arg)
+                if selected_fellow in self.allocations:
+                    return "This fellow has already been allocated a room"
+                else:
+                    for room in self.all_rooms:
+                        print(room)
+                        if room.keys() == ['OFFICE']:
+                            for room_nam in self.offices:
+                                room_nam = {room.values(): ()}
+                                if len(room_nam) < 6:
+                                    room_nam[room.values()].append(tuple(selected_fellow))
+                                    print(room_nam[room.values()])
+                                else:
+                                    return "Room is already full"
+                        elif room.keys() == "LIVING_SPACE":
+                            if len(room) < 4:
+                                room.append(selected_fellow)
+                            else:
+                                return "Living_space is already occupied"
+                if self.Role.upper() == "STAFF":
+                    pass
 
     def rellocate_person(self, PersonID, Room_name):
         pass
@@ -111,3 +133,9 @@ class Amity():
 
     def load_state(self):
         pass
+
+
+amity = Amity()
+amity.create_room("Hogwarts", "o")
+amity.add_person("PAUL", "UPENDO", "FELLOW", "Y")
+print (amity.allocate_room_randomly())
