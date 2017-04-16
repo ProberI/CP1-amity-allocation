ce8ff0a834ead31a5d2a451d83e32ab75cb5680f
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-04-16 03:04:26 +0300

    Add docopt

--- app/cp/view.py
+++ app/cp/view.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
+Amitié is an interactive commandline application to help you allocate rooms to
+Andela Employees randomly and efficiently.
 
 Usage:
-    my_program tcp <host> <port> [--timeout=<seconds>]
-    my_program serial <port> [--baud=<n>] [--timeout=<seconds>]
-    my_program (-i | --interactive)
-    my_program (-h | --help | --version)
+    Amitié create_room <room_type> <room_name>
+    Amitié add_person <first_name> <last_name> <role> <accomodation>
+    Amitié reallocate_person <full_names> <room_name>
+    Amitié load_people <file_name>
+    Amitié print_allocations [--file_name]
+    Amitié unallocated [--file_name]
+    Amitié print_room <room_name>
+    Amitié (-i | --interactive)
+    Amitié (-h | --help | --version)
 
 Options:
     -i, --interactive  Interactive Mode
     -h, --help  Show this screen and exit.
-    --baud=<n>  Baudrate [default: 9600]
 """
 
 import sys
 import cmd
 from docopt import docopt, DocoptExit
+from amity import Amity
 
 
 def docopt_cmd(func):
-    """
-    This decorator is used to simplify the try/except block and pass the result
-    of the docopt parsing to the called action.
-    """
+
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
@@ -51,27 +53,63 @@ def docopt_cmd(func):
     return fn
 
 
-class MyInteractive (cmd.Cmd):
-    intro = 'Welcome to my interactive program!' \
-        + ' (type help for a list of commands.)'
-    prompt = '(my_program) '
+class App(cmd.Cmd):
+    intro = 'Welcome to Amitié!' \
+        + ' (Type help for a list of commands.)'
+    prompt = '(Amitié) '
     file = None
 
     @docopt_cmd
-    def do_tcp(self, arg):
-        """Usage: tcp <host> <port> [--timeout=<seconds>]"""
+    def do_create_room(self, arg):
+        """Usage: create_room <room_type> <room_name>"""
 
-        print(arg)
+        room_type = arg['<room_type>']
+        room_name = arg['<room_name>']
+        amity.create_room(room_type, room_name)
 
     @docopt_cmd
-    def do_serial(self, arg):
-        """Usage: serial <port> [--baud=<n>] [--timeout=<seconds>]
+    def do_add_person(self, arg):
+        """Usage: add_person <first_name> <last_name> <role> <accomodation>"""
 
-Options:
-    --baud=<n>  Baudrate [default: 9600]
-        """
+        first_name = arg['<first_name>']
+        last_name = arg['<last_name>']
+        role = arg['<role>'],
+        accomodation = arg['<accomodation>']
+        amity.add_person(first_name, last_name, role, accomodation)
 
-        print(arg)
+    @docopt_cmd
+    def do_reallocate_person(self, arg):
+        """Usage: reallocate_person <full_names> <room_name>"""
+        person_name = arg['full_names']
+        room_name = arg['<room_name>']
+        amity.reallocate_person(person_name, room_name)
+
+    @docopt_cmd
+    def do_load_people(self, arg):
+        """Usage: load_people <file_name>"""
+        file_name = arg['<file_name>']
+        amity.load_people(file_name)
+
+    @docopt_cmd
+    def do_print_allocations(self, arg):
+        """Usage: print_allocations [--file_name]"""
+        amity.print_allocations()
+
+    @docopt_cmd
+    def do_print_unallocated(self, arg):
+        """Usage: unallocated [--file_name]"""
+        amity.print_unallocated()
+
+    @docopt_cmd
+    def do_print_room(self, arg):
+        """Usage: print_room <room_name>"""
+        room_name = arg['<room_name>']
+        amity.print_room(room_name)
+
+
+# Options:
+#     --baud=<n>  Baudrate [default: 9600]
+#
 
     def do_quit(self, arg):
         """Quits out of Interactive Mode."""
@@ -79,9 +117,11 @@ Options:
         print('Good Bye!')
         exit()
 
+
 opt = docopt(__doc__, sys.argv[1:])
 
 if opt['--interactive']:
-    MyInteractive().cmdloop()
+    amity = Amity()
+    App().cmdloop()
 
 print(opt)
